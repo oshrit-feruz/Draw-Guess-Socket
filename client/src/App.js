999d43bc8ed8cd036df578fd14f9bc6c53e380e7
oshrit-feruz <oshrit13200@gmail.com>
2022-11-09 19:00:45 +0200

    words component and app features

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import Welcome from "./routes/welcome";
+import Words from "./routes/words";
+import Drawing from "./routes/drawing";
+import { Routes, Route } from "react-router-dom";
+// import Container from "./components/container/Container";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Welcome />}></Route>
+        <Route path="/words" element={<Words />}></Route>
+        <Route path="/drawing" element={<Drawing />}></Route>
+        {/* <Route path="/drawing" element={<Container />}></Route>
+        <Route path="/words" element={<WordChoosing />}></Route>
+        <Route path="/waiting" element={<Waiting />}></Route> */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/wordsList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../AppLogo.png";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+export default function WordsList() {
+  return (
+    <>
+    <Button variant="outlined">Primary</Button>
+    </>
+  );
+}
