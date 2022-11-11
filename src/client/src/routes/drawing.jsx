46e0493b91050f793607ed269435960e1e321992
oshrit-feruz <oshrit13200@gmail.com>
2022-11-11 02:42:50 +0200

    F

--- src/client/src/routes/drawing.jsx
+++ src/client/src/routes/drawing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
-import Container from "../components/container/Container";
+import Container from "../components/Container";
 export default function Drawing() {
   return (
     <>
